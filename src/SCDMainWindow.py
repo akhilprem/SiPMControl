28d278ecdd82cfb2913878fbfbe0f1a839d6b03d
Akhil Prem <akhilprem.k@gmail.com>
2016-05-21 18:56:52 +0530

    Added a data model for the DAC table too.

--- src/SCDMainWindow.py
+++ src/SCDMainWindow.py
@@ -10,12 +10,18 @@ class SCDMainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_SCDMainWindow()
         self.ui.setupUi(self)
         
-        self._model = QtGui.QStandardItemModel(48, 2, self)
-        self._model.setHorizontalHeaderLabels(["Reading", "Value"]) # User vert. header to reg #.
-        self.ui.adcTableView.setModel(self._model)
+        self._adcReadOutModel = QtGui.QStandardItemModel(48, 2, self)
+        self._adcReadOutModel.setHorizontalHeaderLabels(["ID/Reading", "Value"]) # User vert. header to reg #.
+        self.ui.adcTableView.setModel(self._adcReadOutModel)
+        
+        self._dacVoltagesModel = QtGui.QStandardItemModel(48, 2, self)
+        self._dacVoltagesModel.setHorizontalHeaderLabels(["ID", "Bias Voltage"])
+        self.ui.dacTableView.setModel(self._dacVoltagesModel)
         
         # Stretch columns to fit available width.
         hHeader = self.ui.adcTableView.horizontalHeader()
+        hHeader.setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        hHeader = self.ui.dacTableView.horizontalHeader()
         hHeader.setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         
         self.show();
