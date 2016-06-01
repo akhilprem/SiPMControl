89c6fc049ce7cc78b19c1e81dc0cb7d6b14e182d
Akhil Prem <akhilprem.k@gmail.com>
2016-06-01 18:25:29 +0530

    1. Added some controls to the window layout like 'Set all BVs', 'Read Periodically' etc.
    2. Merged the I_leak readings with the DAC settings table.

--- src/SCDMainWindow.py
+++ src/SCDMainWindow.py
@@ -13,12 +13,12 @@ class SCDMainWindow(QtWidgets.QMainWindow):
         self.NUM_ROWS = 48  # TBD: This information should come from elsewhere.
         
         self._adcReadOutModel = QtGui.QStandardItemModel(self.NUM_ROWS, 2, self)
-        self._adcReadOutModel.setHorizontalHeaderLabels(["ID/Reading", "Value"]) # User vert. header to reg #.
+        self._adcReadOutModel.setHorizontalHeaderLabels(["Reading", "Value(V)"]) # User vert. header to reg #.
         self.ui.adcTableView.setModel(self._adcReadOutModel)
         
-        self._dacVoltagesModel = QtGui.QStandardItemModel(self.NUM_ROWS, 2, self)
-        self._dacVoltagesModel.setHorizontalHeaderLabels(["ID", "Bias Voltage"])
-        self.ui.dacTableView.setModel(self._dacVoltagesModel)
+        self._channelVoltageCurrentModel = QtGui.QStandardItemModel(self.NUM_ROWS, 3, self)
+        self._channelVoltageCurrentModel.setHorizontalHeaderLabels(["ID", "Bias Voltage(V)", "Leakage Current(uA)"])
+        self.ui.dacTableView.setModel(self._channelVoltageCurrentModel)
         
         # Stretch columns to fit available width.
         hHeader = self.ui.adcTableView.horizontalHeader()
@@ -29,18 +29,21 @@ class SCDMainWindow(QtWidgets.QMainWindow):
         self.ui.adcTableView.verticalHeader().hide()
         self.ui.dacTableView.verticalHeader().hide()
         
-        # Fill column 1 with channel id and make the first column uneditable.
+        # Fill column 1 with channel id and make the first and third column uneditable.
         # Is there a better way to do the latter in one sweep?
         for row in range(self.NUM_ROWS): 
-            index = self._dacVoltagesModel.index(row, 0, QtCore.QModelIndex())
-            self._dacVoltagesModel.setData(index, row)
-            item = self._dacVoltagesModel.itemFromIndex(index)
+            index_0 = self._channelVoltageCurrentModel.index(row, 0, QtCore.QModelIndex())
+            index_2 = self._channelVoltageCurrentModel.index(row, 2, QtCore.QModelIndex())
+            self._channelVoltageCurrentModel.setData(index_0, row)
+            item = self._channelVoltageCurrentModel.itemFromIndex(index_0)
+            item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
+            item = self._channelVoltageCurrentModel.itemFromIndex(index_2)
             item.setFlags(item.flags() & ~QtCore.Qt.ItemIsEditable)
         
 
     def setPresenter(self, presenter):
         self._presenter = presenter
-        self._dacVoltagesModel.itemChanged.connect(self._presenter.handleBVChange)
+        self._channelVoltageCurrentModel.itemChanged.connect(self._presenter.handleBVChange)
 
 
     def changeCurrent(self, channel, current):
